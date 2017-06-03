403dd2cbadc88eae67b6562a4341f0cab5158e71
summychou <summychou@gmail.com>
2017-06-03 18:07:41 +0800

    summychou-20170603/add build.bat

--- LaserQt/lib/qjson/cmake/qjson-qt5/QJSON-qt5Config.cmake
+++
@@ -1,6 +0,0 @@
-GET_FILENAME_COMPONENT(myDir ${CMAKE_CURRENT_LIST_FILE} PATH)
-
-SET(QJSON_LIBRARIES qjson)
-SET(QJSON_INCLUDE_DIR "/home/summy/Cpp-LaserQt/LaserQt/include")
-
-include(${myDir}/QJSON-qt5Targets.cmake)
